f986ac9d47d2edb2502e3e625bbb31d56e151cb9
Sushobhan Nayak <sushobhannayak17@gmail.com>
2024-06-16 21:30:01 +0530

    image add

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:images_picker/images_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -8,6 +13,7 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
+  File? _selectedImage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,6 +60,57 @@ class _ReportScreenState extends State<ReportScreen> {
                 hintText: 'Type your inputs...',
               ),
             ),
+            const SizedBox(
+              height: 11,
+            ),
+            DottedBorder(
+              borderType: BorderType.RRect,
+              radius: const Radius.circular(8),
+              color: const Color(0xffE8E8E8),
+              strokeWidth: 2,
+              child: (_selectedImage == null)
+                  ? Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 140,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffF3F3F3),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Drop images here to upload…'),
+                          MaterialButton(
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                50,
+                              ),
+                            ),
+                            color: const Color(0xffe8e8e8),
+                            onPressed: () async {
+                              final permission = await storagePermission();
+                              getImageGallery();
+                            },
+                            child: const Text('Browse Images'),
+                          )
+                        ],
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: getImageGallery,
+                      child: Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.asset(
+                            _selectedImage!.path,
+                            height: 200,
+                            width: MediaQuery.of(context).size.width,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+            ),
             const Expanded(child: SizedBox()),
             SizedBox(
               width: MediaQuery.of(context).size.width,
@@ -78,5 +135,41 @@ class _ReportScreenState extends State<ReportScreen> {
         ),
       ),
     );
+  }
+
+  Future getImageGallery() async {
+    List<Media>? res = await ImagesPicker.pick(
+      count: 1,
+      pickType: PickType.image,
+    );
+    setState(() {
+      _selectedImage = File(res!.first.path);
+    });
+  }
+
+  Future<bool> storagePermission() async {
+    final DeviceInfoPlugin info = DeviceInfoPlugin();
+    final AndroidDeviceInfo androidInfo = await info.androidInfo;
+    final int androidVersion = int.parse(androidInfo.version.release);
+    bool havePermission = false;
+
+    if (androidVersion >= 13) {
+      final request = await [
+        Permission.videos,
+        Permission.photos,
+      ].request();
+
+      havePermission =
+          request.values.every((status) => status == PermissionStatus.granted);
+    } else {
+      final status = await Permission.storage.request();
+      havePermission = status.isGranted;
+    }
+
+    if (!havePermission) {
+      await openAppSettings();
+    }
+
+    return havePermission;
   }
 }
